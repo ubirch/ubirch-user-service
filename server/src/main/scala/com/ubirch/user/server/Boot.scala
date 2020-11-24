4fad3dc5ebc3e606d39af4c758844678856d8b04
lotta <lotta.rueger@ubirch.com>
2020-11-24 12:22:38 +0100

    changing logging dependencies adding *.iml to .gitignore

--- server/src/main/scala/com/ubirch/user/server/Boot.scala
+++ server/src/main/scala/com/ubirch/user/server/Boot.scala
@@ -2,17 +2,15 @@ package com.ubirch.user.server
 
 import java.util.concurrent.TimeUnit
 
-import com.typesafe.scalalogging.slf4j.StrictLogging
-
-import com.ubirch.user.config.{Config, ConfigKeys}
-import com.ubirch.user.server.route.MainRoute
-import com.ubirch.util.mongo.connection.MongoUtil
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import com.ubirch.user.config.{Config, ConfigKeys}
+import com.ubirch.user.server.route.MainRoute
+import com.ubirch.util.mongo.connection.MongoUtil
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -40,7 +38,6 @@ object Boot extends App with StrictLogging {
 
     logger.info(s"start http server on $interface:$port")
     Http().bindAndHandle((new MainRoute).myRoute, interface, port)
-
   }
 
   private def registerShutdownHooks(): Unit = {
